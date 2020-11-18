ca25e3bd1e80a3e14b0fade5c0db899c3ea18fa7
abdelp <juniorperezpy@gmail.com>
2020-11-18 18:01:18 -0300

    Remove again @sandwich instace variable as indicated in teh book

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,17 +1,19 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'And ideal sandwich' do
-  before { @sandwich = Sandwich.new('delicious', []) }
-
   it 'is delicious' do
-    taste = @sandwich.taste
+    sandwich = Sandwich.new('delicious', [])
+
+    taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'cheese'
-    toppings = @sandwich.toppings
+    sandwich = Sandwich.new('delicious', [])
+
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
 
     expect(toppings).not_to be_empty
   end
