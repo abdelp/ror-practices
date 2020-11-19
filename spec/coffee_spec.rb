b2cfbc8ca1d2708fa354e340e2810a295dd1fffd
abdelp <juniorperezpy@gmail.com>
2020-11-19 10:58:18 -0300

    Remove fcontext from coffee specs

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe 'A cup of coffee' do
     expect(coffee.price).to eq(1.00)
   end
 
-  fcontext 'with milk' do
+  context 'with milk' do
     before { coffee.add :milk }
 
     it 'costs $1.25' do
