97d3578f5fe7d8fa5468443f99290664d3fd7540
Abdel P <abdel.perezpy@gmail.com>
2021-07-02 10:17:12 -0400

    Add user specs

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -4,5 +4,10 @@ require 'webdrivers'
 describe 'User index', js: true do
   it 'show the user index page' do
     visit users_path
+    expect(page).to have_current_path(users_path)
+    click_link 'New User'
+    expect(page).to have_current_path(new_user_path)
+    click_button 'Create User'
   end
 end
+
