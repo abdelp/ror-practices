6260ce5b128a702ae1cd1e4076f5738b9a1a4b36
Abdel P <abdel.perezpy@gmail.com>
2021-07-15 10:15:24 -0400

    Add songs and lyrics configuration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_154603) do
+ActiveRecord::Schema.define(version: 2021_07_15_141002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "people", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "lyrics"
+    t.integer "num_beats"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
